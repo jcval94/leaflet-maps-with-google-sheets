d0d3932d3c99c2532295ce2f2b28e07f9ed04c28
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-12-30 15:49:38 -0600

    Update google-doc-url.js
    share link

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_xjrH3gSImzRKqIkhRAqpAAMjH387yaBWfGbidkKtqM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1lHB5DsqSNjs531puniMolKIQuTMzj5NKyagJCEQqEkk/edit?usp=sharing';
